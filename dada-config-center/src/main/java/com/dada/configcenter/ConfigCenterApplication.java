126d8906af3b4abb417b8b6f72979fd51e85e2f6
tianjun.wang <tianjun.wang@ymm56.com>
2018-08-31 09:51:59 +0800

    'addaconfig'

--- dada-config-center/src/main/java/com/dada/configcenter/ConfigCenterApplication.java
+++ dada-config-center/src/main/java/com/dada/configcenter/ConfigCenterApplication.java
@@ -1,9 +1,12 @@
-package com.dada.configcenter.configcenter;
+package com.dada.configcenter;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigCenterApplication {
 
     public static void main(String[] args) {
