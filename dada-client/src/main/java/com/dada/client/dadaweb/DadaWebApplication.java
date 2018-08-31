2f224cffb1ccdbd78474758a7197477830dde672
tianjun.wang <tianjun.wang@ymm56.com>
2018-08-31 13:50:14 +0800

    'fixconfig'

--- dada-client/src/main/java/com/dada/client/dadaweb/DadaWebApplication.java
+++ dada-client/src/main/java/com/dada/client/dadaweb/DadaWebApplication.java
@@ -1,4 +1,4 @@
-package com.dada.web.dadaweb;
+package com.dada.client.dadaweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
